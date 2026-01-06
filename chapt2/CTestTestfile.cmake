700f1524ed9f85bea66e59943a4ad362083d3bf8
Bei <bei06081221@gmail.com>
2026-01-06 14:40:14 +0800

    day2

--- chapt2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bei/ros2_learn/chapt2
-# Build directory: /home/bei/ros2_learn/chapt2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
